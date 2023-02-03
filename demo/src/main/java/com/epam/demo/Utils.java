cc8dbc0d2fc43b2003a3e3a7b99e8a73b9178353
Vadimzakreva <vadim@sinai.io>
2023-02-03 23:46:29 +0200

    init commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,17 @@
 package com.epam.demo;
 
 import java.util.List;
+import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        if (args == null) return false;
+        for (String str : args) {
+            if (isPositiveNumber(str) == false) {
+                return false;
+            };
+        }
+        return true;
     }
 }
