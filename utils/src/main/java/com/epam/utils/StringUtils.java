b99cf45e769ce0e5f4a241d1c73bd78a4f57107a
Vadimzakreva <vadim@sinai.io>
2023-02-03 20:04:55 +0200

    init commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,19 @@
 package com.epam.utils;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.regex.*;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        Pattern pattern = Pattern.compile("0\\d+");
+        if (pattern.matcher(str).matches()) return false;
+        NumberFormat format = NumberFormat.getInstance();
+        try {
+            Number value = format.parse(str);
+            return (value.floatValue() > 0);
+        } catch (ParseException e) {
+            return false;
+        }
     }
 }
