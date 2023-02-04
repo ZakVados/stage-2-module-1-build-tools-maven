91c2414b9bf230f5f2c301a6d8d404e39944cddc
Vadimzakreva <vadim@sinai.io>
2023-02-04 14:40:13 +0200

    added sanity check

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import java.util.regex.*;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null) return false;
         Pattern pattern = Pattern.compile("0\\d+");
         if (pattern.matcher(str).matches()) return false;
         NumberFormat format = NumberFormat.getInstance();
